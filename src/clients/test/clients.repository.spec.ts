8d492cb17633eac5aba139c91672735758e8f8d6
TothLuna <eladio.feijoo@gmail.com>
2020-09-11 20:43:44 -0400

    add verify id referrer to  clientsRepository #3

--- src/clients/test/clients.repository.spec.ts
+++ src/clients/test/clients.repository.spec.ts
@@ -4,7 +4,8 @@ import { Client } from '../client.interface';
 import { ClientsRepository } from '../clients.repository';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { ClientEntity } from '../client.entity';
-import { clientDto, clientDtoSaved, idNoExist, updateRes, deleteRes } from './data-test'
+import { clientDto, clientDtoSaved, idNoExist, updateRes, deleteRes, referrer } from './data-test'
+import { ClientDto } from '../dtos';
 
 describe('UserService', () => {
   let repository: Repository<Client>;
@@ -36,21 +37,51 @@ describe('UserService', () => {
     expect(clientsRepository).toBeDefined();
   });
 
-  it('save ', async () => {
-    
-    const createSpy = jest.spyOn(repository, 'create')
-    const saveSpy = jest.spyOn(repository, 'save').mockResolvedValue(clientDtoSaved);
+  describe('Save new client', () => {
+    it('save ', async () => {
+      jest.spyOn(repository, 'findOne').mockResolvedValue(referrer)
+      const createSpy = jest.spyOn(repository, 'create')
+      const saveSpy = jest.spyOn(repository, 'save').mockResolvedValue(clientDtoSaved);
+      const result = await clientsRepository.create(clientDto).toPromise();
+      expect(repository.findOne).lastCalledWith(referrer.id);
+      expect(repository.findOne).toBeCalledTimes(1);
+      expect(createSpy).toBeCalledWith({ ...clientDto });
+      expect(saveSpy).toBeCalledTimes(1);
+      expect(result.id).toBeDefined();
+    });
 
-    const pipeMock = {
-      pipe: jest.fn()
-    }
+    it('save client without referrer ', async () => {
 
-    const pipeSpy = jest.spyOn(pipeMock, 'pipe');
+      const clientWithoutDto: ClientDto = {
+        name: 'client test',
+        rif: 'J-3041933-7',
+      }
 
-    const result = await clientsRepository.create(clientDto).toPromise();
-    expect(createSpy).toBeCalledWith({ ...clientDto });
-    expect(saveSpy).toBeCalledTimes(1);
-    expect(result.id).toBeDefined();
+      jest.spyOn(repository, 'findOne')
+      const createSpy = jest.spyOn(repository, 'create')
+      const saveSpy = jest.spyOn(repository, 'save').mockResolvedValue(clientDtoSaved);
+      const result = await clientsRepository.create(clientWithoutDto).toPromise();
+      expect(repository.findOne).not.toBeCalled()
+      expect(createSpy).toBeCalledWith({ ...clientWithoutDto });
+      expect(saveSpy).toBeCalledTimes(1);
+      expect(result.id).toBeDefined();
+    });
+
+    it('error trying to save with a referrer that does not exist  ', async () => {
+      const createClient = {
+        name: 'client test',
+        rif: 'J-3041933-7',
+        referrerId: idNoExist
+      }
+      jest.spyOn(repository, 'findOne').mockResolvedValue(null)
+      try {
+        const result = await clientsRepository.create(createClient).toPromise();
+      } catch (e) {
+        expect(e).toBeDefined();
+        expect(repository.findOne).lastCalledWith(idNoExist);
+        expect(repository.findOne).toBeCalledTimes(1);
+      }
+    });
   });
 
   describe('get', () => {
