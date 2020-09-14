178957a61d4c1a30d2b6cb9a6b83faa70ef4c135
TothLuna <eladio.feijoo@gmail.com>
2020-09-14 16:39:40 -0400

    add status to ClientEntity and create ClientReadExDto #6

---
+++ src/clients/dtos/client-read-ex.dto.ts
@@ -0,0 +1,10 @@
+import { Exclude } from 'class-transformer';
+import { IntersectionType } from '@nestjs/swagger';
+import { ClientReadDto } from '.';
+import { StatusDto } from './status.dto';
+
+@Exclude()
+export class ClientReadExDto extends IntersectionType(
+  ClientReadDto,
+  StatusDto
+  ) {}
---
+++ src/clients/dtos/status.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Exclude, Expose } from "class-transformer";
+import { IsEnum, IsString } from "class-validator";
+import { Status } from "../status.enum";
+
+@Exclude()
+export class StatusDto{
+    @ApiProperty({ enum: ['Active', 'Inactive']})
+    @Expose()
+    @IsEnum(Status)
+    status: string
+}
